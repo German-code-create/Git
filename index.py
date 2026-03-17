8dae433d7828b13a8386d39927ee422a602b90e8
German-code-create <german28177734@gmail.com>
2026-03-17 16:02:45 +0300

    delet

--- index.py
+++ index.py
@@ -17,24 +17,6 @@ class BankAccount:
             except ValueError:
                 print("Ошибка. Введите число")
 
-    def withdraw(self):
-        while True:    
-            try:
-                amount = float(input("Сколько снять с баланса? "))
-
-                if amount > self.deposit:
-                    print("Ошибка. Невозможно снять больше денег, чем есть на счете")
-                    print(f"Вы хотели снять: {amount}. Текущий баланс {self.deposit}")
-                elif amount <= 0:
-                    print("Ошибка. Вы ввели отрицательное число")
-                    print("Попробуйте еще раз")
-                else:    
-                    self.deposit = self.deposit - amount
-                    print(f"Вы сняли с баланса: {amount}. Текущий баланс: {self.deposit}")
-                    break
-            except ValueError:            
-                print("Ошибка. Введите число")
-            
     def show_balance(self):
         print("Текущий баланс: ", self.deposit)
 print("Как дела")
